9f40ae6de524e5400498a692eb9947e84576b0a2
stbordoni <stbordoni@gmail.com>
2020-04-20 14:32:01 +0200

    First commit

---
+++ ArapucaRoutineMainFunctions.py
@@ -0,0 +1,123 @@
+import numpy as np
+import pandas as pd
+import os
+import re
+
+
+from scipy.signal import find_peaks
+
+from ArapucaRoutineUtils import *
+#from RoutineDrawUtils import *
+
+
+def parse_file_name( base_path, file_name):
+    p = re.compile(os.path.join(base_path,     
+    'run(?P<run_number>\d+)_evt(?P<run_part>\d+)_mod304_ch(?P<channel>\d+).dat') )
+
+    m = p.match(file_name)
+    d = m.groupdict()
+    d['file_name'] = file_name
+    return d
+
+
+
+def file_for_run_and_channel(dictt, run=1234567, ch=4):
+    return [ f['file_name'] for f in dictt if f['run_number'] == str(run) and f['channel'] == str(ch)]
+
+
+def file_for_channel(dictt, ch=4):
+    return [ f['file_name'] for f in dictt if f['channel'] == str(ch)]
+
+
+
+
+def readfile_list(filename_list):       
+    
+    def read_single_file(f):
+        #load the file as a np array
+        data = np.loadtxt(f)
+        #data = pd.read_fwf(f, header=None)
+        nevts = int(len(data)/2024)
+        #reshape the array as 10000 events and 2024 columns each
+        data = data.reshape(nevts, 2024)
+        #create a dataframe from the numpy array
+        return pd.DataFrame(data, columns=range(0,2024))
+    
+    data_list = [read_single_file(f) for f in filename_list]
+    return pd.concat(data_list, axis=0)
+
+
+
+
+def create_dataset_list(file_name_dict):
+    
+    df_list = []
+    
+    for i in range (0,2):
+        print('loading files for channel ', i)
+        tmp_filelist = file_for_channel(file_name_dict, ch=i)
+        
+        df_tmp = readfile_list(tmp_filelist)
+        #print(type(df_tmp))
+        
+        df_list.append(df_tmp)
+        
+    return df_list
+
+
+
+
+
+
+def doPreProcessing(df_list):
+    df_list.copy()
+    
+    # pre-processing
+    df_list = prepare_dataset(df_list)
+    
+    print('preprocessing done!')
+
+    return df_list
+
+
+
+
+def doEventSelection(df_list, rundir, csvdir):
+
+    df_list.copy()
+    
+    #create a unique data-set
+    df_allch = pd.concat(df_list, axis=0)
+    
+    #sort values per event and channel
+    df_allch.sort_values(by=['evt number', 'Ch'], inplace=True)
+    
+    #create a new column on the dataframe to check if the event is saturated (the event NOT the waveform)
+    df_allch['isSaturatedEvent'] = (df_allch.groupby(['evt number'])['Saturated'].sum() != 0)
+    
+    # determine if an event has signal
+    df_allch['EventhasSignal'] = (df_allch.groupby(['evt number'])['hasSignal'].sum() != 0)
+    
+    
+    #flag events which are not saturated and have signals has 'Good Events'
+    # these are the events on which we will perform the analysis
+    df_allch['isGoodEvt'] = (df_allch['isSaturatedEvent'] == False) & (df_allch['EventhasSignal'] == True)
+    
+    #print how many events have been passing the Event Selection
+    print('Percentage of events passing the selection', 
+          df_allch.groupby(['evt number'])['isGoodEvt'].first().value_counts()[1] / df_list[0].shape[0] )
+    
+    
+    #save the processed dataframe in csv file
+    #df_allch_packed = pack_waveform(df_allch)
+
+    #suffix = '.csv'
+    #base_filename = 'df_allch_' + rundir+'_processed'
+    #filename = os.path.join(csvdir, base_filename + suffix)
+    #print(filename)
+    
+    #df_allch_packed.to_csv(filename)
+
+    #print('Event selection : done!')
+    return df_allch
+
---
+++ ArapucaRoutineUtils.py
@@ -0,0 +1,161 @@
+import numpy as np
+import pandas as pd 
+import os
+
+from scipy.signal import find_peaks
+
+
+ped   = list(range(100, 200))   # to estimate the pedestal
+rowin = list(range(24, 2024))   # all readout window
+wf    = list(range(250, 1500))  # waveform 
+tail  = list(range(1700, 2000)) # to estimate the single p.e.
+pe    = list(range(200, 800)) # to estimate the single p.e.
+
+
+
+#
+#
+##
+def defineRoI():
+    
+    print('Defined Region of Interest: ' )
+    print('Pedestal region (ped)  : [', min(ped)  , ',' , max(ped)  , ']'  ) 
+    print('Readout window (rowin) : [', min(rowin), ',' , max(rowin), ']'  ) 
+    print('Waveform (wf)          : [', min(wf)   , ',' , max(wf)   , ']'  ) 
+    print('single p.e (pe)        : [', min(pe)   , ',' , max(pe)   , ']'  ) 
+    print('Waveform tail (tail)   : [', min(tail) , ',' , max(tail) , ']'  ) 
+
+    return ped, rowin, wf, pe
+
+
+
+
+#
+# Routine to prepare datasets: check for possible saturation and compute, subtract pedestal 
+##
+def prepare_dataset(df_list):
+    
+    mydflist = [] 
+    ich = 0
+    
+    for _df in df_list: 
+       
+        ich = ich+1 
+        print('preparing dataframe for channel : ', ich)
+
+        _df = define_channel(_df)
+        _df = do_reindex(_df)
+        _df = flag_saturated(_df)
+        _df = compute_pedestal(_df)
+        _df = subtract_pedestal(_df)
+        _df = remove_noise(_df) 
+        _df = has_signal(_df)
+        _df = compute_singlepe(_df)
+
+        mydflist.append(_df)
+        
+
+    print('done!')
+        
+        
+    return mydflist
+
+
+
+def define_channel(df):
+    df = df.copy()
+    df.insert(0, 'Ch', int(df.iloc[0,5]) )   # column 5 in each dataset is the channel number
+    return df
+
+
+
+#
+#  reindex the dataframe to prepare the final concat
+## 
+def do_reindex(df):
+    df=df.copy()
+    df.insert(1, 'evt number', int(df.iloc[0,3]) )  # --> it's the 2nd entry of the header but since there is a ch now inserted I should shift of 1 unit
+    return df
+    #return df.reset_index().rename(columns={"index": "evt number"})
+
+
+
+#
+# Flag the saturated waveform
+###
+def flag_saturated(df, val=10000):
+    df=df.copy()
+    #df['Saturated']=(df.iloc[:, rowin].max(axis=1) >= val)
+    df['Saturated']=(df[rowin].max(axis=1) >= val)
+    return df
+
+
+
+def compute_pedestal(df):
+    df=df.copy()
+    df['Pedestal']=df[ped].sum(axis=1)/len(ped)
+    return df
+
+
+def subtract_pedestal(df):
+    df.copy()
+    df[rowin] = df[rowin].subtract(df['Pedestal'], axis=0)
+    return df
+
+
+#
+#  Apply rolling mean to remove noise. A winodow of 20 seems to work fine (checked also :5,8,10, 15)
+##
+def remove_noise(df):
+    df=df.copy()
+    df[rowin]= df[rowin].rolling(window=20,  axis=1).mean()
+    return df
+
+
+def has_peak(x):
+    peaks, properties = find_peaks(x, height=[15,800], width=10)
+    return (len(peaks) > 0)
+
+
+
+def has_signal(df):
+    df.copy()
+    df['hasSignal'] = df.apply(lambda x: has_peak(x[wf]), axis=1)
+    return df
+
+
+def find_singlePE(x, myrange):
+
+    #rowin = list(range(200,1500))
+    #rowin = list(range(200, 800))
+
+    x = x[min(myrange) : max(myrange)]
+    
+    peaks, properties = find_peaks(x, height=[10,30], width=30, distance = 50)    
+    
+    peaks = peaks+min(myrange)
+    
+    npeaks = len(peaks)  
+    if (npeaks > 0):
+        height = properties['peak_heights'][0]
+        width  = properties['widths'][0]
+        xlow   = int(properties['left_ips'][0])
+        xhigh  = int(properties['right_ips'][0])
+        area   = x[xlow :xhigh].sum() 
+    else :
+        height = 0
+        width  = 0
+        area   = 0
+        
+    return pd.Series([npeaks, height, width, area], index=['n pe', 'pe height', 'pe width', 'pe area'])
+
+
+
+def compute_singlepe(df):
+    df.copy()
+
+    df_pe = df.apply(lambda x: find_singlePE(x, pe), axis=1)    
+    df = pd.concat([df, df_pe], axis =1)
+    
+    return df
+
